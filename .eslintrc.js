00ac11e41413e163e0d4014b84cc7b55e1ae0512
zhangyyysss <690313615@qq.com>
2021-03-02 23:50:24 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,26 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // 文件末尾不为空行
+    'eol-last': 0,
+    // 数字2表示同意缩进2个空格,数字1表示1倍缩进
+    'vue/script-indent': ['error', 2, {'baseIndent': 1}],
+    // {}对象内部强制有空格
+    'object-curly-spacing': ["error", "always"],
+    // 不用强制使用const,可以使用let,var
+    "prefer-const": "off",
+    //function左括号(可以没空格
+    "space-before-function-paren": 1
+  },
+  overrides:[
+    {
+      'files':['*.vue'],
+      'rules':{
+        'indent':['off', 2]
+      }
+    }
+  ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,83 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-block">
+            <input type="text" name="title" required lay-verify="required" placeholder="请输入用户名" autocomplete="off"
+                   class="layui-input">
+          </div>
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input type="password" name="title" required lay-verify="required" placeholder="请输入密码" autocomplete="off"
+                   class="layui-input">
+          </div>
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-inline">
+            <input type="text" name="title" required lay-verify="required" placeholder="请输入验证码" autocomplete="off"
+                   class="layui-input">
+          </div>
+          <div class="layui-form-mid svg" v-html="captcha" @click="getCaptcha()">辅助文字</div>
+        </div>
+        <button type="button" class="layui-btn">立即登录</button>
+        <a href="http://www.layui.com" class="immoc-link">找回密码</a>
+      </form>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import axios from 'axios'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    name: 'app',
+    data () {
+      return {
+        captcha: ''
+      }
+    },
+    mounted () {
+      this.getCaptcha()
+    },
+    methods: {
+      getCaptcha () {
+        axios.get('http://localhost:3000/getCaptcha').then(res => {
+          if (res.status === 200) {
+            let obj = res.data
+            if (obj.code === 200) {
+              this.captcha = res.data.data
+            }
+          }
+        })
+      }
     }
   }
-}
+</script>
+
+<style lang="scss" scoped>
+  #app {
+    background-color: cornsilk;
+  }
+
+  .layui-container {
+    background-color: #fff;
+  }
+
+  input {
+    width: 190px;
+  }
+
+  .immoc-link {
+    margin-left: 10px;
+
+    &:hover {
+      color: blue;
+    }
+  }
+
+  .svg {
+    position: relative;
+    top: -15px;
+  }
 </style>
